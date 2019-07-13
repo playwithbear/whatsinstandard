f7cd98904272bf9d232616941c94bed7781c5a01
DrJonBear <goldspinky@hotmail.com>
2019-07-13 16:23:34 +0100

    EOD Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,9 @@
-// Calling the local JSON file
+// Sets information for rotation logic
 
-let local = new XMLHttpRequest();
-local.open('GET', 'https://github.com/playwithbear/whatsinstandard-pokemon/blob/master/api/local.json', true);
+const in2019 = ["sm1", "sm2", "sm3", "sm35", "sm4"];
+const newSet = [{"code": "sm11", "name": "Unified Minds", "releaseDate": "08/02/2019"}];
 
-local.onload = function () {
-
-  let dataLocal = JSON.parse(this.response);
-
-  console.log('hello');
-}
-
-// Rest of the API Calls and page building
+// API Calls and page building
 const app = document.getElementById('set-display');
 
 const setContainer = document.createElement('ul');
@@ -22,52 +15,52 @@ let request = new XMLHttpRequest();
 request.open('GET', 'https://api.pokemontcg.io/v1/sets/', true);
 request.onload = function () {
 
-    let data = JSON.parse(this.response);
+  let data = JSON.parse(this.response);
 
-    data = data.sets;
-    
-    if (request.status >= 200 && request.status < 400) {
-        
-        //console.log(data.length);
-        //console.log(data[9].name);
+  data = data.sets;
 
-        data.forEach(set => {
-            
-            if (set.standardLegal === true) {
-                //console.log(set.name);
-                //console.log(set.symbolUrl);
+  if (request.status >= 200 && request.status < 400) {
 
-                // Create a list item with the class of item
-                const item = document.createElement('li');
-                item.setAttribute('class','item');
+    //console.log(data.length);
+    //console.log(data[9].name);
 
-                // Add the title of the set to the list item
-                item.textContent = set.name;
+    data.forEach(set => {
 
-                // Add a span containing an img with link to the set logo
-                const setLogoSpan = document.createElement('span');
-                setLogoSpan.setAttribute('class','set-logo');
-                const setLogo = document.createElement('img');
-                setLogo.setAttribute('src',set.symbolUrl);
-                let altText = set.name + ' Set Icon';
-                setLogo.setAttribute('alt',altText);
-                
-                // Append image to span
-                setLogoSpan.appendChild(setLogo);
+      if (set.standardLegal === true) {
+        //console.log(set.name);
+        //console.log(set.symbolUrl);
 
-                // Append span to list item
-                item.appendChild(setLogoSpan);
+        // Create a list item with the class of item
+        const item = document.createElement('li');
+        item.setAttribute('class', 'item');
 
-                // Append list item to set-list <ul>
-                setContainer.appendChild(item);
+        // Add the title of the set to the list item
+        item.textContent = set.name;
 
-            }
+        // Add a span containing an img with link to the set logo
+        const setLogoSpan = document.createElement('span');
+        setLogoSpan.setAttribute('class', 'set-logo');
+        const setLogo = document.createElement('img');
+        setLogo.setAttribute('src', set.symbolUrl);
+        let altText = set.name + ' Set Icon';
+        setLogo.setAttribute('alt', altText);
 
-        });
+        // Append image to span
+        setLogoSpan.appendChild(setLogo);
 
-      } else {
-        console.log('error')
+        // Append span to list item
+        item.appendChild(setLogoSpan);
+
+        // Append list item to set-list <ul>
+        setContainer.appendChild(item);
+
       }
+
+    });
+
+  } else {
+    console.log('error')
+  }
 
 }
 
