2a354863793154367c980e4625b55bbe005bc8f6
DrJonBear <goldspinky@hotmail.com>
2019-07-18 22:26:25 +0100

    EOD commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
 
+// Import components
 import Navbar from "./Navbar"
+
+// Import page elements
+import icon from "../img/icon-original.svg"
 
 export class Header extends Component {
     render() {
         return (
             <header>
-                <img id={"site-icon"} src={require("../img/icon-original.svg")} alt={"What's in Standard? logo"} />
-                <h1>What's in Standard?</h1>
-                <div>
-                    <small class="text-muted">for</small>
-                    <span>Pokémon TCG</span>
-                    <small class="text-muted"> / </small>
-                    <a href="http://whatsinstandard.com/" class="text-muted" rel="noopener">Magic: The Gathering</a>
-                    <small class="text-muted"> / </small>
-                    <a href="http://standardhearth.com/" class="text-muted" rel="noopener">Hearthstone</a>
+                <div className="grid-header">
+                    <div className="grid-header-logo">
+                        <img id="site-icon" src={icon} alt="What's in Standard? logo" />
+                        <h1>What's in Standard?</h1>
+                        <small className="text-muted">for</small>
+                        <span> Pokémon TCG</span>
+                        <small className="text-muted"> / </small>
+                        <a href="http://whatsinstandard.com/" class="text-muted" rel="noopener">Magic: The Gathering</a>
+                        <small className="text-muted"> / </small>
+                        <a href="http://standardhearth.com/" className="text-muted" rel="noopener">Hearthstone</a>    
+                    </div>
+                    <div className="grid-header-pokemon">
+                        
+                    </div>
+                    <Navbar />     
                 </div>
-                <Navbar />
+                
+                <hr/>
             </header>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
     render() {
         return (
-            <nav>
-                
+            <nav className="grid-header-nav">
+
             </nav>
         )
     }
